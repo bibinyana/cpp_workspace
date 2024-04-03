d2bc21c5b359118717cfff45d52a3e32484e2966
bibinc <bibincnair1@gmail.com>
2024-04-03 15:40:01 +0000

    This closes #3

--- include/leeter/remove_element_27.hpp
+++ include/leeter/remove_element_27.hpp
@@ -2,7 +2,8 @@
 class Solution {
 public:
   int removeElement(std::vector<int>& nums, int val) {
-
+    // In place solution using two-pointer approach.
+    // O(n) time and O(1) space.
     int j = 0;
     for(int i = 0; i < nums.size(); i++){
       if(nums[i] != val){
