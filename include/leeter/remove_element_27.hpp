9542a941b897762253e7c59d8404a4988c56fdc1
bibinc <bibincnair1@gmail.com>
2024-04-03 15:37:42 +0000

    This solves #3

--- include/leeter/remove_element_27.hpp
+++ include/leeter/remove_element_27.hpp
@@ -1,5 +1,15 @@
 #include <vector>
 class Solution {
 public:
-  int removeElement(std::vector<int>& nums, int val) {}
+  int removeElement(std::vector<int>& nums, int val) {
+
+    int j = 0;
+    for(int i = 0; i < nums.size(); i++){
+      if(nums[i] != val){
+        nums[j] = nums[i];
+        ++j;
+      }
+    }
+    return j;
+  }
 };
