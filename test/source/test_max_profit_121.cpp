32a9efcd507a5369c5f04c320a9ade4d9e6438d6
bibinc <bibincnair1@gmail.com>
2024-05-27 13:36:44 +0000

    rotate array

---
+++ test/source/test_max_profit_121.cpp
@@ -0,0 +1,16 @@
+#include <doctest/doctest.h>
+#include "leeter/max_profit_121.hpp"
+
+TEST_CASE("max profit: test case 1"){
+    vector<int> input = {7,1,5,3,6,4};
+    Solution sol;
+    int output = sol.maxProfit(input);
+    CHECK(output == 5);
+}
+
+TEST_CASE("max profit: test case 2"){
+    vector<int> input = {7,6,4,3,1};
+    Solution sol;
+    int output = sol.maxProfit(input);
+    CHECK(output == 0);
+}
