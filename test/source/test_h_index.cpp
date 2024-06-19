8d36a2fc31b56f224cb6262ebff4317bdb39b71b
bibin <bibincnair1@gmail.com>
2024-06-19 09:11:04 +0200

    h_index

---
+++ test/source/test_h_index.cpp
@@ -0,0 +1,17 @@
+#include <doctest/doctest.h>
+#include "leeter/h_index.hpp"
+
+
+TEST_CASE("h index: test case 1") {
+  vector<int> input = {3, 0, 6, 1, 5};
+  h_index::Solution sol;
+  int output = sol.hIndex(input);
+  CHECK(output == 3);
+}
+
+TEST_CASE("h index: test case 2") {
+  vector<int> input = {1, 3, 1};
+  h_index::Solution sol;
+  int output = sol.hIndex(input);
+  CHECK(output == 1);
+}
